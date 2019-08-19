60f6cc9aa03e8a9add1708d02b0b4a752cee1f83
Vitalii <besteffects@gmail.com>
2019-08-19 02:20:52 -0400

    printf formatting

---
+++ src/OutputFormatting.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/*
+("%-15s%03d%n", s1, x) % : used as a formatter. '-' : minus sign used for left indentation of the string.
+15s : here 15 denotes the string's minimum field width 15. '0' : pads the extra 0s in the integer.
+3d : here 3 denotes integer's minimum field width 3. %n : prints the new line.
+Sample input:
+java 100
+cpp 65
+python 50
+Reference https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
+ */
+
+public class OutputFormatting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+
+    }
+}
