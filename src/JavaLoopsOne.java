859b4138447c5fa0a48f8c2d4c51a0bf806bc5ad
Vitalii <besteffects@gmail.com>
2019-08-20 01:56:27 -0400

    Loops1

---
+++ src/JavaLoopsOne.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JavaLoopsOne {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int result=0;
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1; i<=10;i++){
+            result=N*i;
+            System.out.println(N + " x " + i + " = " + result);
+            // alternative  System.out.printf("%d x %d = %d\n", n, i, n*i);
+        }
+        scanner.close();
+
+
+    }
+}
