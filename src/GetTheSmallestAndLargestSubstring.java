94e52c20f258a49250c5bfe1f7eec97ea456dcc5
besteffects <besteffects@gmail.com>
2019-12-21 00:58:27 -0500

    GetTheSmallestAndLargestSubstring solution

--- src/GetTheSmallestAndLargestSubstring.java
+++ src/GetTheSmallestAndLargestSubstring.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class GetTheSmallestAndLargestSubstring {
     //string for testing welcometojava
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
+        String smallest = s.substring(0, k);
         String largest = "";
 
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
-//        s = "welcometojava";
-//        char[] word = s.toCharArray();
-//        for (int i = 0; i < word.length-k+1; i++) {
-//            System.out.println(s.subSequence(0, 3));
-//        }
+        // s = "welcometojava";
+        // char[] word = s.toCharArray();
+        String funct;
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            funct = s.substring(i, i + k);
+            if (smallest.compareTo(funct) > 0) smallest = funct;
+            if (largest.compareTo(funct) < 0) largest = funct;
+        }
         return smallest + "\n" + largest;
     }
-
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
