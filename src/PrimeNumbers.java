c5cce51a18416b404009906cbebb8d08f58904b2
besteffects <besteffects@gmail.com>
2020-01-03 01:09:11 -0500

    BigIntegerTask

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -22,22 +22,22 @@ Sample Output
 
 prime
  */
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class PrimeNumbers {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         String n = scanner.nextLine();
-
+        BigInteger number = new BigInteger(n);
+        if (number.isProbablePrime(1)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
         scanner.close();
     }
-   // public boolean isProbablePrime(int certainty)
 }
 
