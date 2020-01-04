90c582080d371f872750a20d38a73097f8299fef
besteffects <besteffects@gmail.com>
2020-01-03 23:33:56 -0500

    ComparatorJava task. Added compare override

--- src/ComparatorJava.java
+++ src/ComparatorJava.java
@@ -20,7 +20,8 @@ Constraints
 - Player names consist of lowercase English letters.
 
 Output Format
-You are not responsible for printing any output to stdout. The locked stub code in Solution will create a Checker object, use it to sort the Player array, and print each sorted element.
+You are not responsible for printing any output to stdout. The locked stub code in Solution will create a Checker object,
+use it to sort the Player array, and print each sorted element.
 
 Sample Input
 5
@@ -37,5 +38,51 @@ david 100
 aakansha 75
 heraldo 50
  */
-public class ComparatorJava {
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+// Write your Checker class here
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b) {
+        if (a.score == b.score) {
+            return a.name.compareTo(b.name);//alphabetically
+        } else {
+            return b.score - a.score;//decreasing order
+        }
+    }
+}
+
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class ComparatorJava {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(); //number of Player objects
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
 }
