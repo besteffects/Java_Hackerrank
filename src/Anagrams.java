d2aec691c873caac6fccf7889007bebd3b717889
Vit <besteffects@gmail.com>
2020-01-05 23:23:29 -0500

    Anagrams task. Solution1

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,4 +1,8 @@
-import java.util.Scanner; /*
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
 Two strings, a and b, are called anagrams if they contain all the same characters in the same frequencies. For example, the
 anagrams of CAT are CAT, ACT, TAC, TCA, ATC, and CTA.
 
@@ -29,8 +33,46 @@ public class Anagrams {
 
     static boolean isAnagram(String a, String b) {
         // Complete the function
-        return false; //should be fixed
+        if (a.length() != b.length()) {
+            return false;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        // populate a map with letters and frequencies of String b
+        Map<Character, Integer> map = new HashMap<>();
+        for (int k = 0; k < b.length(); k++) {
+            char letter = b.charAt(k);
+
+            if (!map.containsKey(letter)) {
+                map.put(letter, 1);
+            } else {
+                Integer frequency = map.get(letter);
+                map.put(letter, ++frequency);
+            }
+        }
+
+// test each letter in String a against data in the map
+// return if letter is absent in the map or its  frequency is 0
+// otherwise decrease the frequency by 1
+
+        for (int k = 0; k < a.length(); k++) {
+            char letter = a.charAt(k);
+
+            if (!map.containsKey(letter))
+                return false;
+
+            Integer frequency = map.get(letter);
+
+            if (frequency == 0)
+                return false;
+            else
+                map.put(letter, --frequency);
+        }
+// if the code got that far it is an anagram
+        return true;
     }
+
 
     public static void main(String[] args) {
 
@@ -39,6 +81,6 @@ public class Anagrams {
         String b = scan.next();
         scan.close();
         boolean ret = isAnagram(a, b);
-        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
     }
 }
